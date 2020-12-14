55e1e4be548790e77108edf48bf98ae7e0a67393
abhiram-anand <anandabhiram0.ar@gmail.com>
2020-12-14 20:24:54 +0530

    js

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+    
+    
+});
+
+
+
+
+
+// checking
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+	}, 3000);
+	
+});
+
+
+
+
+
+
+
+
+
+
